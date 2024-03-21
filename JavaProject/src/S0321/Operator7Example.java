1a54bbaf28484bf22a1e39b48d18982b27e605b0
21-manic-004g <21-manic-004g@21-manic-004>
2024-03-21 17:00:43 +0900

    0321

---
+++ JavaProject/src/S0321/Operator7Example.java
@@ -0,0 +1,19 @@
+package S0321;
+
+public class Operator7Example {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 0;
+		b = ~a;
+		
+		System.out.println(a);
+		System.out.println(b);
+		 
+		b= ~b;
+		
+		System.out.println(b);
+
+	}
+
+}
---
+++ JavaProject/src/S0321/PPExample.java
@@ -0,0 +1,18 @@
+package S0321;
+
+public class PPExample {
+	public static void main(String[] args) {
+		int A=5;
+		
+		System.out.println(A++);
+		System.out.println(A);  
+		
+		System.out.println("---------------");
+		
+		int B=5;
+		
+		System.out.println(++B);
+		System.out.println(B);  
+	}
+
+}
