a457f3aa262eafc6253c7e3d4d41af88baa0ba94
21-manic-004g <21-manic-004g@21-manic-004>
2024-03-21 11:09:52 +0900

    commit 0320

---
+++ JavaProject/src/S0320/FirstClass.java
@@ -0,0 +1,11 @@
+package S0320;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		System.out.println("경기도 파주시 소곡로30");
+	
+	
+	}
+
+}
---
+++ JavaProject/src/S0320/VariableExample.java
@@ -0,0 +1,15 @@
+package S0320;
+
+public class VariableExample {
+
+	public static void main(String[] args) {
+		int value = 10;
+		int result = value + 10;
+		
+		System.out.println(value);
+		System.out.println(result);
+
+	}
+
+}
+ 
